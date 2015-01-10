258f163a341e0edd231c45fcff1a91614480072c
Wenda Wang <wwd7086@gmail.com>
2015-01-10 15:29:38 -0500

    modify the shell

--- InstallBasic.sh
+++ InstallBasic.sh
@@ -1,20 +1,20 @@
-sudo apt-get update
+apt-get update
 
 echo 'install the basics'
-sudo apt-get install lib32stdc++6
-sudo apt-get install lib32ncurses5
-sudo apt-get install lsb
+apt-get install lib32stdc++6
+apt-get install lib32ncurses5
+apt-get install lsb
 
 echo 'install the qtcreator'
-sudo apt-get install qtcreator
+apt-get install qtcreator
 
 echo 'install the toolchain'
-unzip gcc-linaro.zip
-sudo mv gcc-linaro /usr/local
+unzip gcc-linaro.zip
+mv gcc-linaro /usr/local
 
 echo 'install the qt library'
 unzip qt-5.2.zip
-sudo mv qt-5.2 /usr/local
+mv qt-5.2 /usr/local
 
 echo 'start the qt and set the qt kit...'
 qtcreator
